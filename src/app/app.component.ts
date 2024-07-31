0b51fe07a6496c19e5de80617ed54b589962f521
MateusL21 <mateusleonel27@gmail.com>
2024-07-31 20:27:02 -0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { HeaderComponent } from "./components/header/header.component";
 import { ProductComponent } from "./components/product/product.component";
 import { ProductsComponent } from "./components/products/products.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, ProductComponent, ProductsComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cadastro-product-angular';
